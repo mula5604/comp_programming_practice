e3afb5175c614a195fe27f596788e3bc799895cf
mula5604 <emirmba@gmail.com>
2026-09-21 23:53:09 +0200

    tried to solve automat couldnt

--- Automat/main.cpp
+++ Automat/main.cpp
@@ -1,10 +1,58 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
-
-
+#include <algorithm>
 
 int main(){
+
+    int K{};
+    int N{};
+    int M{};
+    int X{};
+    int Y{};
+    int C{};
+
+    std::cin>>N>>M>>K;
+    std::cin>>X>>Y;
+
+    std::vector<int> A(N);
+    std::vector<int> B(M);
+
+    for (int i{};i < N;i++){
+        std::cin>>A[i];
+    }
+
+    for (int i{};i < M;i++){
+        std::cin>>B[i];
+    }
+
+    std::sort(B.begin(),B.end());
+
+    for (int i{};i < M;i++){
+        int bills = (B[i] + K - 1) / K;
+
+        if (Y >= bills){
+            Y -= bills;
+            X += bills * K - B[i];
+            C++;
+        }
+        else{
+            break;
+        }
+    }
+
+    std::sort(A.begin(),A.end());
+
+    for (int i{};i < N;i++){
+        if (X >= A[i]){
+            X -= A[i];
+            C++;
+        }
+        else{
+            break;
+        }
+    }
+
+    std::cout<<C;
 
     return 0;
 }
